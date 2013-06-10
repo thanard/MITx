dc013339658f8e0574958cca422b411ff599d0d6
Thanard Kurutach <kurutach@mit.edu>
2013-06-10 11:35:49 -0400

    save2

--- day1.js
+++ day1.js
@@ -1,6 +1,23 @@
-function calculate(){
-    var input = $('#text1:first');
-    var val = input.val();
-    var output = $('#text_out:first');
-    output.text(val)
+function calculate(text){
+    var pattern=/\d+|\+|\-|\*|\/|\(|\)/g;
+    var tokens = text.match(pattern);
+    return JSON.stringify(tokens);
     }
+    
+function setup_cal(div){
+    var input=$('<input></input>',{type:"text",size: 50});
+    var output = $('<div></div>');
+    var button = $('<button>Calculate</button>');
+    button.bind("click",function(){
+        output.text(String(calculate(input.val())));
+    });
+    
+    $(div).append(input,button,output);
+}
+// #-id .-class
+$(document).ready(function(){
+   $('.calculator').each(function(){
+       //this refers to the <div> with class calculator
+       setup_cal(this);
+   });
+});
