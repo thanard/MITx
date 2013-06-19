097f11342585016caa559f5459d34d55c578e34b
Thanard Kurutach <kurutach@mit.edu>
2013-06-19 11:02:38 -0400

    reverse the chart

--- day8/visual.js
+++ day8/visual.js
@@ -7,29 +7,33 @@ var data =[0,4,8,8,15,16,23,42];
 // data.forEach(function(d){
 // 	chart.append($("<div></div>").css("width",d*10+"px").text(d))
 // })
-var chart_height=140;
+var chart_height=300;
+var chart_width=300;
 
-var x_scale=d3.scale.linear()
-	.domain([0,d3.max(data)]).range(["0%","100%"]);
+var y_scale=d3.scale.linear()
+	.domain([0,d3.max(data)]).range([0,chart_height]);
 
-var y_scale=d3.scale.ordinal()
+var x_scale=d3.scale.ordinal()
 	.domain(d3.keys(data)).rangeBands([0,chart_height]);
 
-var chart= d3.select(".chart-container").append("svg").attr("class","chart");
+var chart= d3.select(".chart-container").append("svg")
+	.attr("class","chart")
+	.attr("height",chart_height)
+	.attr("width",chart_width);
 
 chart.selectAll("rect").data(data)
 	.enter().append("rect")
-	.attr("width",x_scale)
-	.attr("height",20)
-	.attr("y",function(d,i){return y_scale(i);});//20*i
+	.attr("width",x_scale.rangeBand())
+	.attr("height",y_scale)
+	.attr("x",function(d,i){return x_scale(i);})
+	.attr("y",function(d){return chart_height-y_scale(d);});//20*i
 
 chart.selectAll("text").data(data)
 	.enter().append("text")
-	.attr("x",x_scale)
-	.attr("y",function(d,i){return y_scale(i)+y_scale.rangeBand()/2;})//(i+.5)*20;
-	.attr("dx",-3)
-	.attr("dy","0.35em")
-	.attr("text-anchor","end")
+	.attr("y",function(d){return chart_height-y_scale(d)+3;})
+	.attr("x",function(d,i){return x_scale(i)+x_scale.rangeBand()/2;})//(i+.5)*20;
+	.attr("dy","0.7em")
+	.attr("text-anchor","middle")
 	.text(function(d){return d;});
 //select all div bound to data enter choose the one that is new, and then apppend to the same thing
 //add text,style
